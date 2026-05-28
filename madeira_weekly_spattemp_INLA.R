c933323a0e6816cd5e1d62faec2791874420e485
ebrowning <ella.browning.14@ucl.ac.uk>
2026-05-28 09:17:28 +0100

    Update madeira_weekly_spattemp_INLA.R
    Updated model following inclusion of data from 2022-2025.

--- madeira_weekly_spattemp_INLA.R
+++ madeira_weekly_spattemp_INLA.R
@@ -1,6 +1,6 @@
 library(data.table); library(dplyr); library(INLA);library(terra);library(ggplot2)
-#inla.upgrade(testing=TRUE)
-source("00_custom_functions_4_inla_outputs.R")
+#inla.upgrade(testing=FALSE)
+source('00_custom_functions_4_inla_outputs.R')
 ################################################################################
 
 ################################################################################
@@ -10,52 +10,54 @@ data = fread("madeira_data_esa_weather_weekly.csv")
 
 ################################################################################
 colnames(data)
+
+data$hmax_6lag[data$hmax_6lag == "-Inf"] <- NA
+data$hmax_4lag[data$hmax_4lag == "-Inf"] <- NA
+data$psum_6lag[data$psum_6lag == "-Inf"] <- NA
+data$max_wind_intensity[data$max_wind_intensity == "-Inf"] <- NA
+
 # scale all covariates for model
 data = data %>%
+  dplyr::filter(value < 9999) %>% # remove these rows - not valid records.
   dplyr::mutate(year_month = as.Date(paste(year, paste(month, "01", sep = "-"), sep = "-")),
                 year_week = paste(year, week.x, sep = "_"),
                 X_UTM_rescaled = (X_UTM - min(X_UTM))/1000,
                 Y_UTM_rescaled = (Y_UTM - min(Y_UTM))/1000, 
-                altitude = scale(Z_m),
-                elevation = scale(ele_vals...2.),
-                tree_50m = scale(proportion_treecover_50m ), 
-                shrub_50m = scale(proportion_shrubland_50m ),
-                grass_50m = scale(proportion_grassland_50m ),
-                bspveg_50m = scale(proportion_baresparseveg_50m),
-                crop_50m = scale(proportion_cropland_50m ), 
-                builtup_50m = scale(proportion_builtup_50m ), 
-                water_50m = scale(proportion_permwaterbodies_50m ), 
-                tree_100m = scale(proportion_treecover_100m ), 
-                shrub_100m = scale(proportion_shrubland_100m ),
-                grass_100m = scale(proportion_grassland_100m ),
-                bspveg_100m = scale(proportion_baresparseveg_100m),
-                crop_100m = scale(proportion_cropland_100m ), 
-                builtup_100m = scale(proportion_builtup_100m ), 
-                water_100m = scale(proportion_permwaterbodies_100m ),
-                tree_500m = scale(proportion_treecover_500m ), 
-                shrub_500m = scale(proportion_shrubland_500m ),
-                grass_500m = scale(proportion_grassland_500m ), 
-                bspveg_500m = scale(proportion_baresparseveg_500m),
-                crop_500m = scale(proportion_cropland_500m ), 
-                builtup_500m = scale(proportion_builtup_500m ), 
-                water_500m = scale(proportion_permwaterbodies_500m),
-                temp_anomaly = scale(era5_sfc_temperature_2m), 
-                prec_anomaly = scale(era5_sfc_precipitation), 
-                hum_anomaly = scale(era5_sfc_humidity),
-                population_census = scale(population_census), 
-                disp_income = scale(disp_income_2015),
-                estat_pop_1km = scale(estat_pop_1km),
-                intercept = 1) %>%
-  mutate_at(c(62:134), funs(c(scale(.)))) # scales weather vars
+                altitude = base::scale(Z_m),
+                elevation = base::scale(ele_vals...2.),
+                tree_500m = base::scale(proportion_treecover_500m ), 
+                shrub_500m = base::scale(proportion_shrubland_500m ),
+                grass_500m = base::scale(proportion_grassland_500m ), 
+                bspveg_500m = base::scale(proportion_baresparseveg_500m),
+                crop_500m = base::scale(proportion_cropland_500m ), 
+                builtup_500m = base::scale(proportion_builtup_500m ), 
+                water_500m = base::scale(proportion_permwaterbodies_500m),
+                tree_100m = base::scale(proportion_treecover_100m ), 
+                shrub_100m = base::scale(proportion_shrubland_100m ),
+                grass_100m = base::scale(proportion_grassland_100m ), 
+                bspveg_100m = base::scale(proportion_baresparseveg_100m),
+                crop_100m = base::scale(proportion_cropland_100m ), 
+                builtup_100m = base::scale(proportion_builtup_100m ), 
+                water_100m = base::scale(proportion_permwaterbodies_100m),
+                temp_anomaly = base::scale(era5_sfc_temperature_2m), 
+                prec_anomaly = base::scale(era5_sfc_precipitation), 
+                hum_anomaly = base::scale(era5_sfc_humidity),
+                disp_income_2015 = base::scale(disp_income_2015),
+                hmax_6lag = base::scale(hmax_6lag), 
+                max_temp_1.5m = base::scale(max_temp_1.5m), 
+                max_wind_intensity = base::scale(max_wind_intensity), 
+                psum_6lag = base::scale(psum_6lag), 
+                psum_4lag = base::scale(psum_4lag), 
+                hmax_4lag = base::scale(hmax_4lag), 
+                human_pop_500 = base::scale(human_pop_500),
+                intercept = 1)# %>%
+#mutate_at(c(63:124), funs(c(scale(.)))) # scales weather vars
 
 colnames(data)
+head(data)
 
 # some further data sorting 
-colnames(data)[c(8:10, 136:137)] <- c("agency_responsible", "location", "in_out", "gross_income", "income_tax")
-
-data$gross_income <- gsub(" ", "", data$gross_income)
-data$gross_income <- scale(as.numeric(data$gross_income))
-
+colnames(data)[c(8:10)] <- c("agency_responsible", "location", "in_out")
 data$agency_responsible = as.factor(data$agency_responsible)
 data$in_out = as.factor(data$in_out)
 data$ID.year = as.numeric(data$year) - 2012 +1
@@ -77,17 +79,12 @@ table(data$year, data$month) # few records at the beginning of 2012
 
 ggplot(data, aes(x = year_week, y = value, colour = year)) +geom_point() 
 
-data <- dplyr::filter(data, value < 1000)
 
 ################################################################################
 
 # head(data)
-# table(data$year, data$month) # few records at the beginning of 2012 - will start from March 
-# # remove Jan and Feb 2012
-# #data <- data[data$year_month > "2012-02-01", ]
-# # remove 2022 as only data for Jan-March and this may be affecting tail end of model
-data <- data[data$year < 2022, ]
-# remove extreme and likely erroneous value
+table(data$year, data$month) 
+
 
 # find the number of years each location has been surveyed
 #locs_yrs <- data |>
@@ -103,9 +100,9 @@ data <- data[data$year < 2022, ]
 data$week.x[data$week.x == 53] <- 52
   
 # double check no covariance 
-lm500m <- lm(value ~ tree_500m + grass_500m + crop_500m + builtup_500m + 
-               max_temp_1.5m + hmax_6lag + psum_6lag + max_wind_intensity + estat_pop_1km + 
-               disp_income + elevation + prec_anomaly,
+lm500m <- lm(log(value+1) ~ tree_500m + grass_500m + crop_500m + builtup_500m + 
+               max_temp_1.5m + hmax_6lag + psum_6lag + max_wind_intensity + human_pop_500 + 
+               disp_income_2015 + elevation + prec_anomaly +temp_anomaly,
              data = data)
 car::vif(lm500m)
   
@@ -135,39 +132,41 @@ mesh_poly2 = fmesher::fm_mesh_2d_inla(boundary = mad_poly,
                           offset = 2*max.edge)
 
 plot(mesh_poly2)
-points(data$longitude.x, data$latitude.x, col = "pink") #add points to visualise
+points(data$longitude, data$latitude, col = "pink") #add points to visualise
+
+# time mesh 
+k <- 14
+mesh.t <- fmesher::fm_mesh_1d(seq(0 + 0.5 / k, 1 - 0.5 / k, length = k))
 
 # Set range prioirs for the spde model
 prior.median.sd = 0.01; prior.median.range = 0.05
 
 spde2 = inla.spde2.pcmatern(mesh_poly2, prior.range = c(prior.median.range, 0.5),
                             prior.sigma = c(prior.median.sd, 0.1), constr = T)
-indexs <- inla.spde.make.index("s", n.spde = spde2$n.spde, n.group = 10)
+indexs <- inla.spde.make.index("s", n.spde = spde2$n.spde, n.group = k)
 
-# time mesh 
-k <- 10
-mesh.t <- fmesher::fm_mesh_1d(seq(0 + 0.5 / k, 1 - 0.5 / k, length = k))
 
-locs = cbind(data$longitude.x, data$latitude.x)
+locs = cbind(data$longitude, data$latitude)
 A = inla.spde.make.A(mesh = mesh_poly2, loc = locs, group = data$ID.year, group.mesh = mesh.t)
 
 #check colnames again for covriates to be included in effects list
-# add intercept column
-data$intercept <- 1
 
 # covs to be used in models 
 mod_covs <- c("intercept", "Id.Novo", "week.x", "month","year", "ID.year", "ID.year.grp",
               "agency_responsible", "location", "in_out", "altitude", "elevation",
-              "max_humid",  "hmax_6lag",
-               "psum_6lag",
+              "max_humid",  "hmax_6lag", "hmax_4lag",
+               "psum_6lag", "psum_4lag",
               "min_temp_1.5m", 
               "max_temp_1.5m", "tmax_1lag", "tmax_2lag", "tmax_3lag", "tmax_4lag", "tmax_6lag",
               "max_wind_intensity", "wmax_1lag", "wmax_2lag", "wmax_3lag", "wmax_4lag", "wmax_6lag",
               "elevation", "time_since_ep",
               "tree_500m", "shrub_500m",             
-              "grass_500m", "bspveg_500m", "crop_500m", "builtup_500m",
-              "water_500m", "temp_anomaly", "prec_anomaly","hum_anomaly", 
-              "disp_income", "estat_pop_1km"
+              "grass_500m",  "crop_500m", "builtup_500m",
+              "tree_100m", "shrub_100m",             
+              "grass_100m", "crop_100m", "builtup_100m", "water_500m",
+              "water_100m", "temp_anomaly", "prec_anomaly","hum_anomaly", 
+              "disp_income_2015", "human_pop_500",
+              "era5_sfc_temperature_2m", "era5_sfc_precipitation"
 )
 
 # Make stacks for and specify hyperparameters for the random walk
@@ -178,41 +177,39 @@ stack_1 <- inla.stack(tag = 'est', # name tag of the stack (e.g. here est = esti
                                    data.frame(data[ ,..mod_covs])))
 my.init = NULL
 
-hyper.rw_temp = list(theta = list(prior="pc.prec", param=c(3, 0.0001)))
-hyper.rw_yr = list(theta = list(prior="pc.prec", param=c(20, 0.00000001)))
-hyper.rw_hum = list(theta = list(prior="pc.prec", param=c(1, 0.0001)))
-hyper.ar_precan = list(rho = list(prior="pc.prec", param=c(0.5, 0.000000000001)))
+## with non-linear climate anomalies
+hyper.rw_yr = list(theta = list(prior="pc.prec", param=c(1, 0.00001)))
+hyper.rw_precan = list(theta = list(prior="pc.prec", param=c(0.5, 0.01)))
+hyper.rw_tempan = list(theta = list(prior="pc.prec", param=c(0.5, 0.01)))
+hyper.rw_temp = list(theta = list(prior="pc.prec", param=c(5, 0.0001)))
 
-
-fx1_500 <- y ~ -1 + intercept +
+fx3_500 <- y ~ -1 + intercept +
   f(week.x, model = "rw2", cyclic = TRUE, group = ID.year, control.group = list(model = "rw2")) +
   f(ID.year, model = "rw2", hyper = hyper.rw_yr) +
   f(s, model = spde2, group = s.group, control.group = list(model = "ar1")) +
   f(max_temp_1.5m, model = "rw2", hyper = hyper.rw_temp) +
-  f(hmax_6lag, model = "rw2", hyper = hyper.rw_hum) + # model fits better without
-  f(prec_anomaly, model = "ar1", hyper = hyper.ar_precan) +
-  estat_pop_1km +
-  elevation +
-  builtup_500m +
+  f(inla.group(temp_anomaly, n = 40), model = "rw2", hyper = hyper.rw_tempan ) +
+  f(inla.group(prec_anomaly, n = 40), model = "rw2", hyper = hyper.rw_precan) +
+  human_pop_500 +
   crop_500m +
   grass_500m +
-  tree_500m +
+  tree_500m + 
   builtup_500m * elevation +
-  psum_6lag +
+  psum_4lag +
+  hmax_4lag +
   max_wind_intensity 
 
-m1_st_500 <- inla(fx1_500, family = "nbinomial", 
-                     control.family = list(link = "log"),
-                     data = inla.stack.data(stack_1), 
-                     control.inla = list(int.strategy='eb', npoints = 21),
-                     control.fixed=list(prec = 1),
-                     control.mode=list(restart=T, theta=my.init),
-                     control.predictor=list(A = inla.stack.A(stack_1), link = 1, compute=TRUE),
-                     control.compute = list(dic = TRUE, waic = TRUE, cpo = TRUE, config = TRUE),
-                     inla.mode = 'experimental')
+m3_st_500 <- inla(fx3_500, family = "nbinomial", 
+                  control.family = list(link = "log"),
+                  data = inla.stack.data(stack_1), 
+                  control.inla = list(int.strategy='eb', npoints = 21),
+                  control.fixed=list(prec = 1),
+                  control.mode=list(restart=T, theta=my.init),
+                  control.predictor=list(A = inla.stack.A(stack_1), link = 1, compute=TRUE),
+                  control.compute = list(dic = TRUE, waic = TRUE, cpo = TRUE, config = TRUE),
+                  inla.mode = 'experimental')
 
-summary(m1_st_500)
-
+summary(m3_st_500)
 ###############################################################################
 
 ## Model posterior predictive distribution for each model 
@@ -253,9 +250,9 @@ post_samp_func <- function(mod, rand_eff, mod_r_eff, s = 1000, n = n, obs_vals){
   # pred.samples[l,]=dbinom(obs_vals[l], 12, samples.m[l,])
   #}
 }
-m2_st_post1000 <- post_samp_func(mod = m1_st_500, rand_eff = "week.x", mod_r_eff = m1_st_500$summary.random$week.x$mean, s = 10000, n = nrow(data), obs_vals = data$value)
+m3_st_post1000 <- post_samp_func(mod = m3_st_500, rand_eff = "week.x", mod_r_eff = m3_st_500$summary.random$week.x$mean, s = 10000, n = nrow(data), obs_vals = data$value)
 
-m2_st_post1000 <- m2_st_post1000 %>%
+m3_st_post1000 <- m3_st_post1000 %>%
   mutate(Year = as.factor(c(rep(2012, 52),
                           rep(2013, 52),
                           rep(2014, 52),
@@ -269,7 +266,7 @@ m2_st_post1000 <- m2_st_post1000 %>%
                           rep(2022, 52))), 
          Week = rep(1:52, 11))
 
-week_trend_p <- ggplot(m2_st_post1000[1:520, ], aes(x = Week, y = post_mean, colour = Year)) + 
+week_trend_p <- ggplot(m3_st_post1000[1:520, ], aes(x = Week, y = post_mean, colour = Year)) + 
   geom_line() +
   geom_ribbon(aes(ymin = post_95lci, ymax = post_95uci, fill = Year), alpha = 0.3) +
   scale_fill_viridis_d() + xlab("Week") + ylab("Posterior week trend") +
@@ -282,9 +279,9 @@ week_trend_p <- ggplot(m2_st_post1000[1:520, ], aes(x = Week, y = post_mean, col
 #ggsave(filename = "madeira_weekyr_trends.png", dpi = 400, width = 20, height = 15, units = "cm")
 
 
-m1_st_post1000_yr <- post_samp_func(mod = m1_st_500, rand_eff = "ID.year", mod_r_eff = m1_st_500$summary.random$ID.year$mean, s = 10000, n = nrow(data), obs_vals = data$value)
+m3_st_post1000_yr <- post_samp_func(mod = m3_st_500, rand_eff = "ID.year", mod_r_eff = m1_st_500$summary.random$ID.year$mean, s = 10000, n = nrow(data), obs_vals = data$value)
 
-m1_st_post1000_yr$Year = (2012:2022)
+m3_st_post1000_yr$Year = (2012:2022)
 ggplot(m1_st_post1000_yr[-11,], aes(x = Year, y = post_mean)) + geom_point() + geom_line() +
   geom_ribbon(aes(ymin = post_95lci, ymax = post_95uci), alpha = 0.3) +
   geom_ribbon(aes(ymin = post_79lci, ymax = post_79uci), alpha = 0.5) +
@@ -293,7 +290,7 @@ ggplot(m1_st_post1000_yr[-11,], aes(x = Year, y = post_mean)) + geom_point() + g
 ################################################################################
 ################################################################################
 
-yr_trnd = m1_st_post1000_yr %>%
+yr_trnd = m3_st_post1000_yr %>%
   mutate(mean_rescaled = (inla.link.invlog(post_mean))/inla.link.invlog(post_mean[2]),
          lower_rescaled = (inla.link.invlog(post_95lci))/inla.link.invlog(post_mean[2]),
          upper_rescaled = (inla.link.invlog(post_95uci))/inla.link.invlog(post_mean[2]),
@@ -308,7 +305,7 @@ year_change_p <- ggplot(yr_trnd[-11,], aes(x = Year, y = mean_rescaled)) + geom_
   #geom_hline( yintercept = yr_trnd$upper_rescaled[2], linetype = "dashed", colour = "grey20")
 
 
-week_trnd <- m2_st_post1000 %>%
+week_trnd <- m3_st_post1000 %>%
   group_by(Week) %>%
   mutate(mean_rescaled = (inla.link.invlog(post_mean))/inla.link.invlog(post_mean[2]),
          lower_rescaled = (inla.link.invlog(post_95lci))/inla.link.invlog(post_mean[2]),
